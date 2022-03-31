71cf809c6298edb0846f0241e05e3d564c7acd9a
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-03-31 22:43:09 +0100

    ok

---
+++ lib/application/auth/sign_up_form/phone/sign_up_form_phone_cubit.freezed.dart
@@ -0,0 +1,273 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'sign_up_form_phone_cubit.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$SignUpFormPhoneStateTearOff {
+  const _$SignUpFormPhoneStateTearOff();
+
+  _SignUpFormPhoneState call(
+      {required Phone phoneNumber,
+      required String smsCode,
+      required Option<AuthFailure> failureOption,
+      required Option<String> verificationIdOption,
+      required bool isSubmitting,
+      required bool showErrorMessages}) {
+    return _SignUpFormPhoneState(
+      phoneNumber: phoneNumber,
+      smsCode: smsCode,
+      failureOption: failureOption,
+      verificationIdOption: verificationIdOption,
+      isSubmitting: isSubmitting,
+      showErrorMessages: showErrorMessages,
+    );
+  }
+}
+
+/// @nodoc
+const $SignUpFormPhoneState = _$SignUpFormPhoneStateTearOff();
+
+/// @nodoc
+mixin _$SignUpFormPhoneState {
+  Phone get phoneNumber => throw _privateConstructorUsedError;
+  String get smsCode => throw _privateConstructorUsedError;
+  Option<AuthFailure> get failureOption => throw _privateConstructorUsedError;
+  Option<String> get verificationIdOption => throw _privateConstructorUsedError;
+  bool get isSubmitting => throw _privateConstructorUsedError;
+  bool get showErrorMessages => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SignUpFormPhoneStateCopyWith<SignUpFormPhoneState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SignUpFormPhoneStateCopyWith<$Res> {
+  factory $SignUpFormPhoneStateCopyWith(SignUpFormPhoneState value,
+          $Res Function(SignUpFormPhoneState) then) =
+      _$SignUpFormPhoneStateCopyWithImpl<$Res>;
+  $Res call(
+      {Phone phoneNumber,
+      String smsCode,
+      Option<AuthFailure> failureOption,
+      Option<String> verificationIdOption,
+      bool isSubmitting,
+      bool showErrorMessages});
+}
+
+/// @nodoc
+class _$SignUpFormPhoneStateCopyWithImpl<$Res>
+    implements $SignUpFormPhoneStateCopyWith<$Res> {
+  _$SignUpFormPhoneStateCopyWithImpl(this._value, this._then);
+
+  final SignUpFormPhoneState _value;
+  // ignore: unused_field
+  final $Res Function(SignUpFormPhoneState) _then;
+
+  @override
+  $Res call({
+    Object? phoneNumber = freezed,
+    Object? smsCode = freezed,
+    Object? failureOption = freezed,
+    Object? verificationIdOption = freezed,
+    Object? isSubmitting = freezed,
+    Object? showErrorMessages = freezed,
+  }) {
+    return _then(_value.copyWith(
+      phoneNumber: phoneNumber == freezed
+          ? _value.phoneNumber
+          : phoneNumber // ignore: cast_nullable_to_non_nullable
+              as Phone,
+      smsCode: smsCode == freezed
+          ? _value.smsCode
+          : smsCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      failureOption: failureOption == freezed
+          ? _value.failureOption
+          : failureOption // ignore: cast_nullable_to_non_nullable
+              as Option<AuthFailure>,
+      verificationIdOption: verificationIdOption == freezed
+          ? _value.verificationIdOption
+          : verificationIdOption // ignore: cast_nullable_to_non_nullable
+              as Option<String>,
+      isSubmitting: isSubmitting == freezed
+          ? _value.isSubmitting
+          : isSubmitting // ignore: cast_nullable_to_non_nullable
+              as bool,
+      showErrorMessages: showErrorMessages == freezed
+          ? _value.showErrorMessages
+          : showErrorMessages // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$SignUpFormPhoneStateCopyWith<$Res>
+    implements $SignUpFormPhoneStateCopyWith<$Res> {
+  factory _$SignUpFormPhoneStateCopyWith(_SignUpFormPhoneState value,
+          $Res Function(_SignUpFormPhoneState) then) =
+      __$SignUpFormPhoneStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {Phone phoneNumber,
+      String smsCode,
+      Option<AuthFailure> failureOption,
+      Option<String> verificationIdOption,
+      bool isSubmitting,
+      bool showErrorMessages});
+}
+
+/// @nodoc
+class __$SignUpFormPhoneStateCopyWithImpl<$Res>
+    extends _$SignUpFormPhoneStateCopyWithImpl<$Res>
+    implements _$SignUpFormPhoneStateCopyWith<$Res> {
+  __$SignUpFormPhoneStateCopyWithImpl(
+      _SignUpFormPhoneState _value, $Res Function(_SignUpFormPhoneState) _then)
+      : super(_value, (v) => _then(v as _SignUpFormPhoneState));
+
+  @override
+  _SignUpFormPhoneState get _value => super._value as _SignUpFormPhoneState;
+
+  @override
+  $Res call({
+    Object? phoneNumber = freezed,
+    Object? smsCode = freezed,
+    Object? failureOption = freezed,
+    Object? verificationIdOption = freezed,
+    Object? isSubmitting = freezed,
+    Object? showErrorMessages = freezed,
+  }) {
+    return _then(_SignUpFormPhoneState(
+      phoneNumber: phoneNumber == freezed
+          ? _value.phoneNumber
+          : phoneNumber // ignore: cast_nullable_to_non_nullable
+              as Phone,
+      smsCode: smsCode == freezed
+          ? _value.smsCode
+          : smsCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      failureOption: failureOption == freezed
+          ? _value.failureOption
+          : failureOption // ignore: cast_nullable_to_non_nullable
+              as Option<AuthFailure>,
+      verificationIdOption: verificationIdOption == freezed
+          ? _value.verificationIdOption
+          : verificationIdOption // ignore: cast_nullable_to_non_nullable
+              as Option<String>,
+      isSubmitting: isSubmitting == freezed
+          ? _value.isSubmitting
+          : isSubmitting // ignore: cast_nullable_to_non_nullable
+              as bool,
+      showErrorMessages: showErrorMessages == freezed
+          ? _value.showErrorMessages
+          : showErrorMessages // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SignUpFormPhoneState extends _SignUpFormPhoneState {
+  const _$_SignUpFormPhoneState(
+      {required this.phoneNumber,
+      required this.smsCode,
+      required this.failureOption,
+      required this.verificationIdOption,
+      required this.isSubmitting,
+      required this.showErrorMessages})
+      : super._();
+
+  @override
+  final Phone phoneNumber;
+  @override
+  final String smsCode;
+  @override
+  final Option<AuthFailure> failureOption;
+  @override
+  final Option<String> verificationIdOption;
+  @override
+  final bool isSubmitting;
+  @override
+  final bool showErrorMessages;
+
+  @override
+  String toString() {
+    return 'SignUpFormPhoneState(phoneNumber: $phoneNumber, smsCode: $smsCode, failureOption: $failureOption, verificationIdOption: $verificationIdOption, isSubmitting: $isSubmitting, showErrorMessages: $showErrorMessages)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _SignUpFormPhoneState &&
+            const DeepCollectionEquality()
+                .equals(other.phoneNumber, phoneNumber) &&
+            const DeepCollectionEquality().equals(other.smsCode, smsCode) &&
+            const DeepCollectionEquality()
+                .equals(other.failureOption, failureOption) &&
+            const DeepCollectionEquality()
+                .equals(other.verificationIdOption, verificationIdOption) &&
+            const DeepCollectionEquality()
+                .equals(other.isSubmitting, isSubmitting) &&
+            const DeepCollectionEquality()
+                .equals(other.showErrorMessages, showErrorMessages));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(phoneNumber),
+      const DeepCollectionEquality().hash(smsCode),
+      const DeepCollectionEquality().hash(failureOption),
+      const DeepCollectionEquality().hash(verificationIdOption),
+      const DeepCollectionEquality().hash(isSubmitting),
+      const DeepCollectionEquality().hash(showErrorMessages));
+
+  @JsonKey(ignore: true)
+  @override
+  _$SignUpFormPhoneStateCopyWith<_SignUpFormPhoneState> get copyWith =>
+      __$SignUpFormPhoneStateCopyWithImpl<_SignUpFormPhoneState>(
+          this, _$identity);
+}
+
+abstract class _SignUpFormPhoneState extends SignUpFormPhoneState {
+  const factory _SignUpFormPhoneState(
+      {required Phone phoneNumber,
+      required String smsCode,
+      required Option<AuthFailure> failureOption,
+      required Option<String> verificationIdOption,
+      required bool isSubmitting,
+      required bool showErrorMessages}) = _$_SignUpFormPhoneState;
+  const _SignUpFormPhoneState._() : super._();
+
+  @override
+  Phone get phoneNumber;
+  @override
+  String get smsCode;
+  @override
+  Option<AuthFailure> get failureOption;
+  @override
+  Option<String> get verificationIdOption;
+  @override
+  bool get isSubmitting;
+  @override
+  bool get showErrorMessages;
+  @override
+  @JsonKey(ignore: true)
+  _$SignUpFormPhoneStateCopyWith<_SignUpFormPhoneState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
