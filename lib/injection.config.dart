d1c5cd9ccf9496b4d97d64ee7ebbf4d7fef5e398
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-04-28 14:10:56 +0100

    ok

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -12,39 +12,41 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import 'application/auth/auth_cubit.dart' as _i3;
-import 'application/auth/reset_password_form/reset_password_cubit.dart' as _i19;
+import 'application/auth/reset_password_form/reset_password_cubit.dart' as _i21;
 import 'application/auth/sign_in_form/email/sign_in_form_email_cubit.dart'
-    as _i21;
+    as _i23;
 import 'application/auth/sign_in_form/phone/sign_in_form_phone_cubit.dart'
-    as _i22;
-import 'application/auth/sign_up_form/email/sign_up_form_cubit.dart' as _i23;
-import 'application/auth/sign_up_form/phone/sign_up_form_phone_cubit.dart'
     as _i24;
+import 'application/auth/sign_up_form/email/sign_up_form_cubit.dart' as _i25;
+import 'application/auth/sign_up_form/phone/sign_up_form_phone_cubit.dart'
+    as _i26;
 import 'domain/auth/i_auth_facade.dart' as _i8;
-import 'domain/auth/i_firestore_facade.dart' as _i10;
-import 'domain/auth/i_functions_facade.dart' as _i12;
-import 'domain/auth/i_storage_facade.dart' as _i14;
+import 'domain/auth/i_external_facade.dart' as _i10;
+import 'domain/auth/i_firestore_facade.dart' as _i12;
+import 'domain/auth/i_functions_facade.dart' as _i14;
+import 'domain/auth/i_storage_facade.dart' as _i16;
+import 'infrastructure/auth/external_facade.dart' as _i11;
 import 'infrastructure/auth/firebase_auth_facade.dart' as _i9;
-import 'infrastructure/auth/firebase_firestore_facade.dart' as _i11;
-import 'infrastructure/auth/firebase_functions_facade.dart' as _i13;
-import 'infrastructure/auth/firebase_storage_facade.dart' as _i15;
-import 'infrastructure/core/firebase_injectible.dart' as _i30;
-import 'presentation/home/dashboard/cubit/dashboard_cubit.dart' as _i29;
+import 'infrastructure/auth/firebase_firestore_facade.dart' as _i13;
+import 'infrastructure/auth/firebase_functions_facade.dart' as _i15;
+import 'infrastructure/auth/firebase_storage_facade.dart' as _i17;
+import 'infrastructure/core/firebase_injectible.dart' as _i32;
+import 'presentation/home/dashboard/cubit/dashboard_cubit.dart' as _i31;
 import 'presentation/home/dashboard/screens/notifications/cubit/notification_cubit.dart'
-    as _i17;
+    as _i19;
 import 'presentation/home/dashboard/screens/payment_method/bank/cubit/bank_address_cubit.dart'
-    as _i27;
+    as _i29;
 import 'presentation/home/dashboard/screens/payment_method/crypto/cubit/crypto_wallet_cubit.dart'
-    as _i28;
+    as _i30;
 import 'presentation/home/dashboard/screens/profile/cubit/profile_cubit.dart'
-    as _i18;
-import 'presentation/home/dashboard/screens/security/cubit/security_cubit.dart'
     as _i20;
+import 'presentation/home/dashboard/screens/security/cubit/security_cubit.dart'
+    as _i22;
 import 'presentation/home/dashboard/screens/verification/cubit/verification_cubit.dart'
-    as _i25;
-import 'presentation/home/investment/cubit/investment_cubit.dart' as _i16;
+    as _i27;
+import 'presentation/home/investment/cubit/investment_cubit.dart' as _i18;
 import 'presentation/home/wallet/cubit/wallet_cubit.dart'
-    as _i26; // ignore_for_file: unnecessary_lambdas
+    as _i28; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -63,42 +65,43 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.lazySingleton<_i8.IAuthFacade>(() => _i9.FirebaseAuthFacade(
       firestore: get<_i5.FirebaseFirestore>(),
       firebaseAuth: get<_i4.FirebaseAuth>()));
-  gh.lazySingleton<_i10.IFirestoreFacade>(() => _i11.FirebaseFirestoreFacade(
+  gh.lazySingleton<_i10.IExternalFacade>(() => _i11.ExternalFacade());
+  gh.lazySingleton<_i12.IFirestoreFacade>(() => _i13.FirebaseFirestoreFacade(
       get<_i5.FirebaseFirestore>(), get<_i4.FirebaseAuth>()));
-  gh.lazySingleton<_i12.IFunctionsFacade>(() => _i13.FirebaseFunctionsFacade());
-  gh.lazySingleton<_i14.IStorageFacade>(() => _i15.FirebaseStorageFacade(
+  gh.lazySingleton<_i14.IFunctionsFacade>(() => _i15.FirebaseFunctionsFacade());
+  gh.lazySingleton<_i16.IStorageFacade>(() => _i17.FirebaseStorageFacade(
       get<_i4.FirebaseAuth>(), get<_i7.FirebaseStorage>()));
-  gh.factory<_i16.InvestmentCubit>(
-      () => _i16.InvestmentCubit(get<_i10.IFirestoreFacade>()));
-  gh.factory<_i17.NotificationCubit>(
-      () => _i17.NotificationCubit(get<_i10.IFirestoreFacade>()));
-  gh.factory<_i18.ProfileCubit>(
-      () => _i18.ProfileCubit(get<_i8.IAuthFacade>()));
-  gh.factory<_i19.ResetPasswordCubit>(
-      () => _i19.ResetPasswordCubit(get<_i8.IAuthFacade>()));
-  gh.factory<_i20.SecurityCubit>(
-      () => _i20.SecurityCubit(get<_i8.IAuthFacade>()));
-  gh.factory<_i21.SignInFormEmailCubit>(() =>
-      _i21.SignInFormEmailCubit(get<_i8.IAuthFacade>(), get<_i3.AuthCubit>()));
-  gh.factory<_i22.SignInFormPhoneCubit>(
-      () => _i22.SignInFormPhoneCubit(get<_i3.AuthCubit>()));
-  gh.factory<_i23.SignUpFormCubit>(
-      () => _i23.SignUpFormCubit(get<_i8.IAuthFacade>()));
-  gh.factory<_i24.SignUpFormPhoneCubit>(
-      () => _i24.SignUpFormPhoneCubit(get<_i3.AuthCubit>()));
-  gh.factory<_i25.VerificationCubit>(() => _i25.VerificationCubit(
-      get<_i14.IStorageFacade>(),
-      get<_i10.IFirestoreFacade>(),
+  gh.factory<_i18.InvestmentCubit>(
+      () => _i18.InvestmentCubit(get<_i12.IFirestoreFacade>()));
+  gh.factory<_i19.NotificationCubit>(
+      () => _i19.NotificationCubit(get<_i12.IFirestoreFacade>()));
+  gh.factory<_i20.ProfileCubit>(
+      () => _i20.ProfileCubit(get<_i8.IAuthFacade>()));
+  gh.factory<_i21.ResetPasswordCubit>(
+      () => _i21.ResetPasswordCubit(get<_i8.IAuthFacade>()));
+  gh.factory<_i22.SecurityCubit>(
+      () => _i22.SecurityCubit(get<_i8.IAuthFacade>()));
+  gh.factory<_i23.SignInFormEmailCubit>(() =>
+      _i23.SignInFormEmailCubit(get<_i8.IAuthFacade>(), get<_i3.AuthCubit>()));
+  gh.factory<_i24.SignInFormPhoneCubit>(
+      () => _i24.SignInFormPhoneCubit(get<_i3.AuthCubit>()));
+  gh.factory<_i25.SignUpFormCubit>(
+      () => _i25.SignUpFormCubit(get<_i8.IAuthFacade>()));
+  gh.factory<_i26.SignUpFormPhoneCubit>(
+      () => _i26.SignUpFormPhoneCubit(get<_i3.AuthCubit>()));
+  gh.factory<_i27.VerificationCubit>(() => _i27.VerificationCubit(
+      get<_i16.IStorageFacade>(),
+      get<_i12.IFirestoreFacade>(),
       get<_i3.AuthCubit>()));
-  gh.factory<_i26.WalletCubit>(
-      () => _i26.WalletCubit(get<_i10.IFirestoreFacade>()));
-  gh.factory<_i27.BankAddressCubit>(
-      () => _i27.BankAddressCubit(get<_i10.IFirestoreFacade>()));
-  gh.factory<_i28.CryptoWalletCubit>(
-      () => _i28.CryptoWalletCubit(get<_i10.IFirestoreFacade>()));
-  gh.factory<_i29.DashboardCubit>(
-      () => _i29.DashboardCubit(get<_i14.IStorageFacade>()));
+  gh.factory<_i28.WalletCubit>(
+      () => _i28.WalletCubit(get<_i12.IFirestoreFacade>()));
+  gh.factory<_i29.BankAddressCubit>(
+      () => _i29.BankAddressCubit(get<_i12.IFirestoreFacade>()));
+  gh.factory<_i30.CryptoWalletCubit>(
+      () => _i30.CryptoWalletCubit(get<_i12.IFirestoreFacade>()));
+  gh.factory<_i31.DashboardCubit>(
+      () => _i31.DashboardCubit(get<_i16.IStorageFacade>()));
   return get;
 }
 
-class _$FirebaseInjectableModule extends _i30.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i32.FirebaseInjectableModule {}
--- lib/presentation/home/investment/payment/bank/bank_invest.dart
+++ lib/presentation/home/investment/payment/bank/bank_invest.dart
@@ -2,8 +2,10 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fortfolio/domain/constants/theme.dart';
+import 'package:fortfolio/domain/widgets/custom_snackbar.dart';
 import 'package:fortfolio/domain/widgets/loading_view.dart';
 import 'package:fortfolio/presentation/home/investment/cubit/investment_cubit.dart';
+import 'package:fortfolio/presentation/routes/router.gr.dart';
 import 'domiciliary.dart';
 import 'naira.dart';
 
@@ -33,90 +35,108 @@ class _BankInvestmentPageState extends State<BankInvestmentPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocSelector<InvestmentCubit, InvestmentState, bool>(
-        selector: (state) {
-          return state.isLoading;
-        },
-        builder: (context, isLoading) {
-          if (isLoading) {
-            return const LoadingView();
-          } else {
-            return SafeArea(
-              child: Padding(
-                padding: kDefaultPadding,
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      InkWell(
-                        onTap: () => context.router.pop(),
-                        child: const Icon(Icons.close),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "Invest Using bank\nTransfer",
-                        style: titleText,
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height,
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              width: MediaQuery.of(context).size.height,
-                              decoration: const BoxDecoration(
-                                color: Colors.transparent,
-                              ),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: TabBar(
-                                      unselectedLabelColor:
-                                          const Color(0XFF656565),
-                                      labelColor: kBlackColor,
-                                      indicator: const BoxDecoration(
-                                          border: Border(
-                                              bottom: BorderSide(
-                                                  color: kPrimaryColor,
-                                                  width: 3.0))),
-                                      controller: _tabController,
-                                      tabs: const [
-                                        Tab(
-                                          text: 'Naira',
-                                        ),
-                                        Tab(
-                                          text: 'Domiciliary',
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              child: TabBarView(
-                                controller: _tabController,
-                                children: const <Widget>[
-                                  NairaAccount(),
-                                  DomiciliaryAccount(),
-                                ],
-                              ),
-                            )
-                          ],
+      body: MultiBlocListener(
+        listeners: [
+              BlocListener<InvestmentCubit, InvestmentState>(
+                listenWhen: (previous, current) => previous.failure != current.failure && current.failure.isNotEmpty,
+              listener: (context, state) {
+                CustomSnackbar.showSnackBar(context, state.failure, true);
+              },
+      
+            ),
+              BlocListener<InvestmentCubit, InvestmentState>(
+                  listenWhen: (previous, current) => previous.success != current.success && current.success.isNotEmpty,
+              listener: (context, state) {
+                CustomSnackbar.showSnackBar(context, state.success, false);
+                context.router.push(const InvestmentSuccessRoute());
+              },
+              ),
+          ],
+        child: BlocSelector<InvestmentCubit, InvestmentState, bool>(
+          selector: (state) {
+            return state.isLoading;
+          },
+          builder: (context, isLoading) {
+            if (isLoading) {
+              return const LoadingView();
+            } else {
+              return SafeArea(
+                child: Padding(
+                  padding: kDefaultPadding,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(
+                          height: 20,
                         ),
-                      ),
-                    ],
+                        InkWell(
+                          onTap: () => context.router.pop(),
+                          child: const Icon(Icons.close),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Invest Using bank\nTransfer",
+                          style: titleText,
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.height,
+                                decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                ),
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(5),
+                                      child: TabBar(
+                                        unselectedLabelColor:
+                                            const Color(0XFF656565),
+                                        labelColor: kBlackColor,
+                                        indicator: const BoxDecoration(
+                                            border: Border(
+                                                bottom: BorderSide(
+                                                    color: kPrimaryColor,
+                                                    width: 3.0))),
+                                        controller: _tabController,
+                                        tabs: const [
+                                          Tab(
+                                            text: 'Naira',
+                                          ),
+                                          Tab(
+                                            text: 'Domiciliary',
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: TabBarView(
+                                  controller: _tabController,
+                                  children: const <Widget>[
+                                    NairaAccount(),
+                                    DomiciliaryAccount(),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
-          }
-        },
+              );
+            }
+          },
+        ),
       ),
     );
   }
