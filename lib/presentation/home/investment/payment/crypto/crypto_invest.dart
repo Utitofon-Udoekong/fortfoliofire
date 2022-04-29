bb70a80634d9968352c8691bfd3c1017f703c868
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-04-30 00:40:43 +0100

    ok

--- lib/presentation/home/investment/payment/crypto/crypto_invest.dart
+++ lib/presentation/home/investment/payment/crypto/crypto_invest.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+import 'dart:io';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
@@ -9,13 +12,18 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:fortfolio/infrastructure/auth/external_facade.dart';
 import 'package:fortfolio/domain/widgets/loading_view.dart';
 import 'package:fortfolio/presentation/home/investment/cubit/investment_cubit.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class CryptoInvestmentPage extends StatelessWidget {
+class CryptoInvestmentPage extends StatefulWidget {
   const CryptoInvestmentPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    InAppWebViewController _webViewController;
+  State<CryptoInvestmentPage> createState() => _CryptoInvestmentPageState();
+}
+
+class _CryptoInvestmentPageState extends State<CryptoInvestmentPage> {
+final GlobalKey webViewKey = GlobalKey();
+    InAppWebViewController? webViewController;
     InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
         crossPlatform: InAppWebViewOptions(
           useShouldOverrideUrlLoading: true,
@@ -27,6 +35,63 @@ class CryptoInvestmentPage extends StatelessWidget {
         ios: IOSInAppWebViewOptions(
           allowsInlineMediaPlayback: true,
         ));
+     late PullToRefreshController pullToRefreshController;
+  late ContextMenu contextMenu;
+  String url = "";
+  double progress = 0;
+  final urlController = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    contextMenu = ContextMenu(
+        menuItems: [
+          ContextMenuItem(
+              androidId: 1,
+              iosId: "1",
+              title: "Special",
+              action: () async {
+                print("Menu item Special clicked!");
+                print(await webViewController?.getSelectedText());
+                await webViewController?.clearFocus();
+              })
+        ],
+        options: ContextMenuOptions(hideDefaultSystemContextMenuItems: false),
+        onCreateContextMenu: (hitTestResult) async {
+          print("onCreateContextMenu");
+          print(hitTestResult.extra);
+          print(await webViewController?.getSelectedText());
+        },
+        onHideContextMenu: () {
+          print("onHideContextMenu");
+        },
+        onContextMenuActionItemClicked: (contextMenuItemClicked) async {
+          var id = (Platform.isAndroid)
+              ? contextMenuItemClicked.androidId
+              : contextMenuItemClicked.iosId;
+          print("onContextMenuActionItemClicked: " +
+              id.toString() +
+              " " +
+              contextMenuItemClicked.title);
+        });
+
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(
+        color: Colors.blue,
+      ),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          webViewController?.reload();
+        } else if (Platform.isIOS) {
+          webViewController?.loadUrl(
+              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+        }
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -76,22 +141,115 @@ class CryptoInvestmentPage extends StatelessWidget {
                         height: 30,
                       ),
                       Expanded(
-                          child: SizedBox(
-                        child: InAppWebView(
-                          initialUrlRequest: URLRequest(
-                              url: Uri.parse("https://www.youtube.com/")),
-                          initialOptions: options,
-                          onWebViewCreated:
-                              (InAppWebViewController controller) {
-                            _webViewController = controller;
-                          },
-                          onReceivedServerTrustAuthRequest:
-                              (controller, challenge) async {
-                            return ServerTrustAuthResponse(
-                                action: ServerTrustAuthResponseAction.PROCEED);
-                          },
-                        ),
-                      )),
+            child: Stack(
+              children: [
+                InAppWebView(
+                  key: webViewKey,
+                  // contextMenu: contextMenu,
+                  initialUrlRequest:
+                      URLRequest(url: Uri.parse("https://github.com/flutter")),
+                  // initialFile: "assets/index.html",
+                  initialUserScripts: UnmodifiableListView<UserScript>([]),
+                  initialOptions: options,
+                  pullToRefreshController: pullToRefreshController,
+                  onWebViewCreated: (controller) {
+                    webViewController = controller;
+                  },
+                  onLoadStart: (controller, url) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  androidOnPermissionRequest:
+                      (controller, origin, resources) async {
+                    return PermissionRequestResponse(
+                        resources: resources,
+                        action: PermissionRequestResponseAction.GRANT);
+                  },
+                  shouldOverrideUrlLoading:
+                      (controller, navigationAction) async {
+                    var uri = navigationAction.request.url!;
+
+                    if (![
+                      "http",
+                      "https",
+                      "file",
+                      "chrome",
+                      "data",
+                      "javascript",
+                      "about"
+                    ].contains(uri.scheme)) {
+                      if (await canLaunchUrl(Uri.parse(url))) {
+                        // Launch the App
+                        await launchUrl(
+                          Uri.parse(url),
+                        );
+                        // and cancel the request
+                        return NavigationActionPolicy.CANCEL;
+                      }
+                    }
+
+                    return NavigationActionPolicy.ALLOW;
+                  },
+                  onLoadStop: (controller, url) async {
+                    pullToRefreshController.endRefreshing();
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onLoadError: (controller, url, code, message) {
+                    pullToRefreshController.endRefreshing();
+                  },
+                  onProgressChanged: (controller, progress) {
+                    if (progress == 100) {
+                      pullToRefreshController.endRefreshing();
+                    }
+                    setState(() {
+                      this.progress = progress / 100;
+                      urlController.text = url;
+                    });
+                  },
+                  onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onConsoleMessage: (controller, consoleMessage) {
+                    print(consoleMessage);
+                  },
+                ),
+                progress < 1.0
+                    ? LinearProgressIndicator(value: progress)
+                    : Container(),
+              ],
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  webViewController?.goBack();
+                },
+              ),
+              ElevatedButton(
+                child: const Icon(Icons.arrow_forward),
+                onPressed: () {
+                  webViewController?.goForward();
+                },
+              ),
+              ElevatedButton(
+                child: const Icon(Icons.refresh),
+                onPressed: () {
+                  webViewController?.reload();
+                },
+              ),
+            ],
+          ),
                       // Container(
                       //   padding: const EdgeInsets.all(8.0),
                       //   decoration: BoxDecoration(
