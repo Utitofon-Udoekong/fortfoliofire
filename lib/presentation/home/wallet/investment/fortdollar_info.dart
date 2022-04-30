24c95f5bb5a51c8ebf118d327e43ec83a6137c37
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-04-30 20:50:54 +0100

    ok

--- lib/presentation/home/wallet/investment/fortdollar_info.dart
+++ lib/presentation/home/wallet/investment/fortdollar_info.dart
@@ -57,8 +57,7 @@ class FortDollarInvestmentInfo extends StatelessWidget {
                       fontSize: 16, fontWeight: FontWeight.w500)),
               const SizedBox(height: 30),
               Text("Active Investments",
-                  style:
-                      subTitle.copyWith(fontSize: 12, color: kPrimaryColor)),
+                  style: subTitle.copyWith(fontSize: 12, color: kPrimaryColor)),
               const SizedBox(height: 15),
               SizedBox(
                 height: activeInvestments.length * 100 + 80,
@@ -69,19 +68,22 @@ class FortDollarInvestmentInfo extends StatelessWidget {
                   scrollDirection: Axis.vertical,
                   itemBuilder: ((context, index) {
                     return buildTile(
-                      title: '${activeInvestments[index].planName} / ${activeInvestments[index].duration.toInt()} month(s)',
-                      amount: '\$${formatter.format(activeInvestments[index].amount)}',
+                      title:
+                          '${activeInvestments[index].planName} / ${activeInvestments[index].duration.toInt()} month(s)',
+                      amount:
+                          '\$${formatter.format(activeInvestments[index].amount)}',
                       ontap: () {
-                      context
-                          .read<WalletCubit>()
-                          .investmentToBeWithdrawnChanged(
-                              investmentToBeWithdrawn:
-                                  activeInvestments[index]);
-                      context.router.push(const WithdrawalPageRoute());
-                    },
+                        context
+                            .read<WalletCubit>()
+                            .investmentToBeWithdrawnChanged(
+                                investmentToBeWithdrawn:
+                                    activeInvestments[index]);
+                        context.router.push(const WithdrawalPageRoute());
+                      },
                       pending: activeInvestments[index].status == "Pending",
                       isDue: activeInvestments[index].dueDate.isToday,
-                      daysLeft: Jiffy(activeInvestments[index].dueDate).fromNow(),
+                      daysLeft:
+                          Jiffy(activeInvestments[index].dueDate).fromNow(),
                     );
                   }),
                 ),
@@ -130,48 +132,50 @@ Widget buildTile(
               )
             ],
           ),
-          isDue ? GestureDetector(
-            onTap: pending ? null : ontap,
-            child: Container(
-              alignment: Alignment.center,
-              height: 48,
-              width: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: pending
-                    ? const Color.fromRGBO(3, 66, 109, 0.65)
-                    : kPrimaryColor,
-              ),
-              child: Text(
-                pending ? 'Pending' : 'Withdraw',
-                style: textButton.copyWith(color: kWhiteColor, fontSize: 15),
-              ),
-            ),
-          ) :
-          Tooltip(
-            message: "Unlocks $daysLeft",
-                triggerMode: TooltipTriggerMode.tap,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  color: kSecondaryColor
-                ),
-                height: 50,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-                alignment: Alignment.center,
-                height: 48,
-                width: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color.fromRGBO(3, 66, 109, 0.65),
-                ),
-                child: Text(
-                  'Withdraw',
-                  style: textButton.copyWith(color: kWhiteColor, fontSize: 15),
-                ),
-              ),
-          )
+          isDue
+              ? GestureDetector(
+                  onTap: pending ? null : ontap,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 48,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: pending
+                          ? const Color.fromRGBO(3, 66, 109, 0.65)
+                          : kPrimaryColor,
+                    ),
+                    child: Text(
+                      pending ? 'Pending' : 'Withdraw',
+                      style:
+                          textButton.copyWith(color: kWhiteColor, fontSize: 15),
+                    ),
+                  ),
+                )
+              : Tooltip(
+                  message: "Unlocks $daysLeft",
+                  triggerMode: TooltipTriggerMode.tap,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: kSecondaryColor),
+                  height: 50,
+                  padding: const EdgeInsets.all(8.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 48,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromRGBO(3, 66, 109, 0.65),
+                    ),
+                    child: Text(
+                      'Withdraw',
+                      style:
+                          textButton.copyWith(color: kWhiteColor, fontSize: 15),
+                    ),
+                  ),
+                )
         ],
       ));
 }
