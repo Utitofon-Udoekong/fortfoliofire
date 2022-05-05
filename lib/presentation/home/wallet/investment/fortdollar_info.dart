3d28085d088d5f4ea5a73af2c8a7b14ef2dc4864
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-05 02:05:18 +0100

    ok

--- lib/presentation/home/wallet/investment/fortdollar_info.dart
+++ lib/presentation/home/wallet/investment/fortdollar_info.dart
@@ -134,7 +134,26 @@ Widget buildTile(
             ],
           ),
           isDue
-              ? Tooltip(
+              ? GestureDetector(
+                  onTap: pending ? null : ontap,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 48,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: pending
+                          ? const Color.fromRGBO(3, 66, 109, 0.65)
+                          : kPrimaryColor,
+                    ),
+                    child: Text(
+                      pending ? 'Pending' : 'Withdraw',
+                      style:
+                          textButton.copyWith(color: kWhiteColor, fontSize: 15),
+                    ),
+                  ),
+                )
+              : Tooltip(
                   message: "Unlocks $daysLeft",
                   triggerMode: TooltipTriggerMode.tap,
                   decoration: BoxDecoration(
@@ -157,26 +176,7 @@ Widget buildTile(
                           textButton.copyWith(color: kWhiteColor, fontSize: 15),
                     ),
                   ),
-                ) : GestureDetector(
-                  onTap: pending ? null : ontap,
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 48,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: pending
-                          ? const Color.fromRGBO(3, 66, 109, 0.65)
-                          : kPrimaryColor,
-                    ),
-                    child: Text(
-                      pending ? 'Pending' : 'Withdraw',
-                      style:
-                          textButton.copyWith(color: kWhiteColor, fontSize: 15),
-                    ),
-                  ),
                 )
         ],
       ));
 }
-
