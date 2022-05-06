c73572cfe832efb8179e954ec528c68eb5e0f2c6
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-06 20:59:59 +0100

    ok

--- lib/infrastructure/auth/dto/investment/investment_dto.dart
+++ lib/infrastructure/auth/dto/investment/investment_dto.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fortfolio/domain/user/investment.dart';
-import 'package:fortfolio/utils/pages.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 
