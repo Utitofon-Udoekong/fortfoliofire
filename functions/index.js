4be611657f7f86380e1cb23f2d6698f86c2cb114
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-14 23:23:46 +0100

    ok

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,15 @@
 require('dotenv').config()
-const functions = require("firebase-functions/v2");
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-// const { response } = require("express");
-const app = admin.initializeApp(functions.config().functions);
-const {getFirestore} = require("firebase-admin/firestore")
-const firestore = getFirestore(app)
-// const cors = require('cors')({ origin: '*' });
+const { response } = require("express");
+admin.initializeApp(functions.config().functions);
+const firestore = admin.firestore()
+const cors = require('cors')({ origin: '*' });
 
-const { Client, Webhook, resources } = require('coinbase-commerce-node');
-const { onMessagePublished } = require('firebase-functions/v2/pubsub');
+const { Client, Webhook } = require('coinbase-commerce-node');
 const coinbaseSecret = process.env.COINBASE_API_KEY;
 Client.init(coinbaseSecret);
 
-const { Charge } = resources;
 
 // exports.scheduleInvestmentFor3Months = functions.pubsub.schedule('0 0 * */3 *').onRun((context) => {
 //     return functions.https.onCall(async (data, context) => {
@@ -20,7 +17,7 @@ const { Charge } = resources;
 //         const docId = data.docId
 //         const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
 //         const documentData = reference.get()
-    
+
 //         const amountInvested = documentData["amount"] ?? 0
 //         const roi = documentData["roi"] ?? 0
 //         const duration = documentData["duration"] ?? 3
@@ -28,7 +25,7 @@ const { Charge } = resources;
 //         const currentRoiForDuration = (roi * duration) / 12
 //         const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
 //         const returnsPerDay = returnsAtEndOfDuration / numberOfDays
-    
+
 //         await reference.update({
 //             planYield: admin.firestore.FieldValue.increment(returnsPerDay)
 //         })
@@ -47,7 +44,7 @@ const { Charge } = resources;
 //         const currentRoiForDuration = (roi * duration) / 12
 //         const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
 //         const returnsPerDay = returnsAtEndOfDuration / numberOfDays
-    
+
 //         await reference.update({
 //             planYield: admin.firestore.FieldValue.increment(returnsPerDay)
 //         })
@@ -59,7 +56,7 @@ const { Charge } = resources;
 //         const docId = data.docId
 //         const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
 //         const documentData = reference.get()
-    
+
 //         const amountInvested = documentData["amount"] ?? 0
 //         const roi = documentData["roi"] ?? 0
 //         const duration = documentData["duration"] ?? 12
@@ -67,7 +64,7 @@ const { Charge } = resources;
 //         const currentRoiForDuration = (roi * duration) / 12
 //         const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
 //         const returnsPerDay = returnsAtEndOfDuration / numberOfDays
-    
+
 //         await reference.update({
 //             planYield: admin.firestore.FieldValue.increment(returnsPerDay)
 //         })
@@ -79,7 +76,7 @@ const { Charge } = resources;
 //         const docId = data.docId
 //         const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
 //         const documentData = reference.get()
-    
+
 //         const amountInvested = documentData["amount"] ?? 0
 //         const roi = documentData["roi"] ?? 0
 //         const duration = documentData["duration"] ?? 12
@@ -87,7 +84,7 @@ const { Charge } = resources;
 //         const currentRoiForDuration = (roi * duration) / 12
 //         const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
 //         const returnsPerDay = returnsAtEndOfDuration / numberOfDays
-    
+
 //         return await reference.update({
 //             planYield: admin.firestore.FieldValue.increment(returnsPerDay)
 //         })
@@ -97,7 +94,7 @@ const { Charge } = resources;
 //         const docId = data.docId
 //         const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
 //         const documentData = reference.get()
-    
+
 //         const amountInvested = documentData["amount"] ?? 0
 //         const roi = documentData["roi"] ?? 0
 //         const duration = documentData["duration"] ?? 6
@@ -105,28 +102,28 @@ const { Charge } = resources;
 //         const currentRoiForDuration = (roi * duration) / 6
 //         const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
 //         const returnsPerDay = returnsAtEndOfDuration / numberOfDays
-    
+
 //         return await reference.update({
 //             planYield: admin.firestore.FieldValue.increment(returnsPerDay)
 //         })
 // }
 async function handle3MonthsInvestmentTask(data, context) {
   const uid = context.auth.uid
-        const docId = data.docId
-        const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
-        const documentData = reference.get()
-    
-        const amountInvested = documentData["amount"] ?? 0
-        const roi = documentData["roi"] ?? 0
-        const duration = documentData["duration"] ?? 3
-        const numberOfDays = documentData["numberOfDays"] ?? 0
-        const currentRoiForDuration = (roi * duration) / 3
-        const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
-        const returnsPerDay = returnsAtEndOfDuration / numberOfDays
-    
-        return await reference.update({
-            planYield: admin.firestore.FieldValue.increment(returnsPerDay)
-        })
+  const docId = data.docId
+  const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
+  const documentData = reference.get()
+
+  const amountInvested = documentData["amount"] ?? 0
+  const roi = documentData["roi"] ?? 0
+  const duration = documentData["duration"] ?? 3
+  const numberOfDays = documentData["numberOfDays"] ?? 0
+  const currentRoiForDuration = (roi * duration) / 3
+  const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
+  const returnsPerDay = returnsAtEndOfDuration / numberOfDays
+
+  return await reference.update({
+    planYield: admin.firestore.FieldValue.increment(returnsPerDay)
+  })
 }
 
 // exports.scheduleInvestmentFor12Months = functions.pubsub.schedule('0 0 * */12 *')
@@ -134,27 +131,27 @@ async function handle3MonthsInvestmentTask(data, context) {
 // exports.scheduleInvestmentFor6Months = functions.pubsub.schedule('0 0 * */6 *')
 //     .onRun((context) => await handle12MonthsInvestmentTask({ scheduled: true }, context));
 exports.scheduleInvestmentFor3Months = functions.pubsub.schedule('0 0 * */3 *')
-    .onRun(async (context) => await handle3MonthsInvestmentTask({}, context));
+  .onRun(async (context) => await handle3MonthsInvestmentTask({}, context));
 
 // exports.invest12Months = functions.https.onCall(handle12MonthsInvestmentTask);
 // exports.invest6Months = functions.https.onCall(handle6MonthsInvestmentTask);
-exports.invest3Months = functions.https.onCall(async (data, context) =>{
+exports.invest3Months = functions.https.onCall(async (data, context) => {
   const uid = context.auth.uid
-        const docId = data.docId
-        const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
-        const documentData = reference.get()
-    
-        const amountInvested = documentData["amount"] ?? 0
-        const roi = documentData["roi"] ?? 0
-        const duration = documentData["duration"] ?? 3
-        const numberOfDays = documentData["numberOfDays"] ?? 0
-        const currentRoiForDuration = (roi * duration) / 3
-        const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
-        const returnsPerDay = returnsAtEndOfDuration / numberOfDays
-    
-        return await reference.update({
-            planYield: admin.firestore.FieldValue.increment(returnsPerDay)
-        })
+  const docId = data.docId
+  const reference = firestore.collection("authUsers").doc(uid).collection("investments").doc(docId)
+  const documentData = reference.get()
+
+  const amountInvested = documentData["amount"] ?? 0
+  const roi = documentData["roi"] ?? 0
+  const duration = documentData["duration"] ?? 3
+  const numberOfDays = documentData["numberOfDays"] ?? 0
+  const currentRoiForDuration = (roi * duration) / 3
+  const returnsAtEndOfDuration = currentRoiForDuration * amountInvested
+  const returnsPerDay = returnsAtEndOfDuration / numberOfDays
+
+  return await reference.update({
+    planYield: admin.firestore.FieldValue.increment(returnsPerDay)
+  })
 });
 
 // exports.endInvestment = functions.firestore.document("/authUsers/{uid}/investments/{docId}").onCreate(async (snapshot, context) => {
@@ -163,7 +160,7 @@ exports.invest3Months = functions.https.onCall(async (data, context) =>{
 //     const dueDate = documentData["dueDate"]
 //     const date =  new Date()
 //     const today = date.toLocaleString()
-    
+
 // })
 
 
@@ -178,27 +175,28 @@ exports.invest3Months = functions.https.onCall(async (data, context) =>{
 //         },
 //         pricing_type: 'fixed_price',
 //       };
-  
+
 //       const charge = await Charge.create(chargeData);
 //       functions.logger.log(charge)
-  
+
 //       res.send(charge);
 //     });
 //   });
 
-  exports.webhookHandler = functions.https.onRequest(async (req, res) => {
+exports.webhookHandler = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
     const rawBody = req.rawBody;
     const signature = req.headers['x-cc-webhook-signature'];
     const webhookSecret = process.env.COINBASE_WEBHOOK_SECRET;
-  
+
     try {
       const event = Webhook.verifyEventBody(rawBody, signature, webhookSecret);
-  
+
       res.send(`${event.id}`);
-      
+
     } catch (error) {
       functions.logger.error(error);
       res.status(400).send('failure!');
     }
-  });
-  
+  })
+});
