3fd1613f5f1b332ef21b14ce5d081ee3e7514db5
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-17 21:51:23 +0100

    ok

--- lib/presentation/home/investment/payment/crypto/crypto_invest.dart
+++ lib/presentation/home/investment/payment/crypto/crypto_invest.dart
@@ -2,12 +2,13 @@ import 'dart:async';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:fortfolio/domain/constants/theme.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fortfolio/domain/widgets/webview/webview_stack.dart';
-// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:fortfolio/presentation/home/investment/cubit/investment_cubit.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import 'package:fortfolio/domain/constants/theme.dart';
+import 'package:fortfolio/domain/widgets/webview/navigation_controller.dart';
+import 'package:fortfolio/domain/widgets/webview/webview_stack.dart';
+import 'package:fortfolio/presentation/home/investment/cubit/investment_cubit.dart';
 
 class CryptoInvestmentPage extends StatefulWidget {
   const CryptoInvestmentPage({Key? key}) : super(key: key);
@@ -33,15 +34,19 @@ class _CryptoInvestmentPageState extends State<CryptoInvestmentPage> {
         context.select((InvestmentCubit cubit) => cubit.state.charge.hostedUrl);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: kWhiteColor,
+        backgroundColor: kPrimaryColor,
         leading: InkWell(
           onTap: () {
             context.read<InvestmentCubit>().cancelCharge();
             context.router.pop();
           },
-          child: const Icon(Icons.close),
+          child: const Icon(Icons.close, color: kWhiteColor,),
         ),
-        title: Text("Invest with Crypto", style: subTitle.copyWith(color: kBlackColor, fontSize: 18),),
+        title: Text("Invest with Crypto", style: subTitle.copyWith(color: kWhiteColor, fontSize: 18),),
+        actions: [
+          NavigationControls(controller: controller),
+        ],
+
       ),
       body: WebviewStack(controller: controller, paymentUrl: paymentUrl)
     );
