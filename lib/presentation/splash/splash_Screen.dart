3c6120791f541dd5f81b7e17257e3ea7015e1673
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-19 16:26:32 +0100

    ok

--- lib/presentation/splash/splash_Screen.dart
+++ lib/presentation/splash/splash_Screen.dart
@@ -32,16 +32,16 @@ class _SplashScreenState extends State<SplashScreen> {
 
     if (isLoggedIn && isChecked) {
       if (Platform.isAndroid) {
+        if (canCheckBiometrics) {
+          bool didauthenticate = await LocalAuthApi.authenticate(localizedReason: 'Scan Fingerprint to authenticate');
+          if (didauthenticate != true) {
+            exit(0);
+          }else{
+            Future.delayed(const Duration(seconds: 1), () {
               context.router.replace(const HomePageRoute());
-        // if (canCheckBiometrics) {
-        //   bool didauthenticate = await LocalAuthApi.authenticate(localizedReason: 'Scan Fingerprint to authenticate');
-        //   if (didauthenticate != true) {
-        //     exit(0);
-        //   }else{
-        //     Future.delayed(const Duration(seconds: 1), () {
-        //     });
-        //   }
-        // }
+            });
+          }
+        }
       }
     } else {
       context.router.replace(const OnboardingScreenRoute());
