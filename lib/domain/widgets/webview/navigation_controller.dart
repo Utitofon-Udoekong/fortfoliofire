ffd0a8cfb9eb495105f9d19f572f7420ae5401ad
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-19 12:23:53 +0100

    ok

--- lib/domain/widgets/webview/navigation_controller.dart
+++ lib/domain/widgets/webview/navigation_controller.dart
@@ -18,9 +18,9 @@ class NavigationControls extends StatelessWidget {
             controller == null) {
           return Row(
             children: const <Widget>[
-              Icon(Icons.arrow_back_ios),
-              Icon(Icons.arrow_forward_ios),
-              Icon(Icons.replay),
+              Icon(Icons.arrow_back_ios, size: 13,),
+              Icon(Icons.arrow_forward_ios, size: 13,),
+              Icon(Icons.replay, size: 13,),
             ],
           );
         }
@@ -28,7 +28,7 @@ class NavigationControls extends StatelessWidget {
         return Row(
           children: <Widget>[
             IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
+              icon: const Icon(Icons.arrow_back_ios, size: 13),
               onPressed: () async {
                 if (await controller.canGoBack()) {
                   await controller.goBack();
@@ -41,7 +41,7 @@ class NavigationControls extends StatelessWidget {
               },
             ),
             IconButton(
-              icon: const Icon(Icons.arrow_forward_ios),
+              icon: const Icon(Icons.arrow_forward_ios, size: 13),
               onPressed: () async {
                 if (await controller.canGoForward()) {
                   await controller.goForward();
@@ -54,7 +54,7 @@ class NavigationControls extends StatelessWidget {
               },
             ),
             IconButton(
-              icon: const Icon(Icons.replay),
+              icon: const Icon(Icons.replay, size: 13),
               onPressed: () {
                 controller.reload();
               },
