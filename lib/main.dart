b34cd89f72e821ed4453f08f5a22cc9ac1aa71d7
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-06-13 22:54:16 +0100

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,6 +20,8 @@ void main() async {
   await FirebaseAppCheck.instance.activate(
     webRecaptchaSiteKey: '6Le-eCcgAAAAAC3IN0RKos4l2gLT60GvLLGoobbq',
   );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
   BlocOverrides.runZoned(() => runApp(const App()),
       blocObserver: AppBlocObserver());
 
