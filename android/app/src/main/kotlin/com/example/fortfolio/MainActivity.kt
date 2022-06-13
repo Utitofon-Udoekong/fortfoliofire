c8c82e759ff81516bf266bbc5ab131219c3150d3
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-06-13 17:41:36 +0100

    ok

--- android/app/src/main/kotlin/com/example/fortfolio/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fortfolio/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.fortfolio
+package com.mega.fortfolio
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
