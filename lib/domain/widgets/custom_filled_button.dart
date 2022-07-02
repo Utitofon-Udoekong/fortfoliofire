1e50d5c6cf9b87a6a96e651bf3a1b5bff8859f02
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-02 21:21:31 +0100

    ok

--- lib/domain/widgets/custom_filled_button.dart
+++ lib/domain/widgets/custom_filled_button.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fortfolio/domain/constants/theme.dart';
 
@@ -9,7 +12,22 @@ class CustomFilledButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    bool isIos = Platform.isIOS;
+    return isIos
+        ? SizedBox(
+            width: double.infinity,
+            child: CupertinoButton(
+              minSize: 45,
+              onPressed: onTap,
+              color: kPrimaryColor,
+              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+              child: Text(
+                text,
+                style: textButton.copyWith(color: kWhiteColor),
+              ),
+            ),
+          )
+        : InkWell(
       onTap: onTap,
       child: Ink(
         child: Container(
