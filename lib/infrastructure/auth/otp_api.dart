94666367a165ba1610d8d7327dc3579ca9d317ed
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-15 20:42:48 +0100

    ok

--- lib/infrastructure/auth/otp_api.dart
+++ lib/infrastructure/auth/otp_api.dart
@@ -1,22 +1,22 @@
-import 'dart:math';
-import 'package:sms/sms.dart';
+// import 'dart:math';
+// import 'package:sms/sms.dart';
 
-class OTPApi {
-  int otp = 0, _minOtpValue = 0, _maxOtpValue = 0;
-  void generateOtp() {
-    _minOtpValue = 100000;
-    _maxOtpValue = 999999;
-    otp = _minOtpValue + Random().nextInt(_maxOtpValue - _minOtpValue);
-  }
+// class OTPApi {
+//   int otp = 0, _minOtpValue = 0, _maxOtpValue = 0;
+//   void generateOtp() {
+//     _minOtpValue = 100000;
+//     _maxOtpValue = 999999;
+//     otp = _minOtpValue + Random().nextInt(_maxOtpValue - _minOtpValue);
+//   }
 
-  void sendOtp({required String phoneNumber}) {
-    generateOtp();
-    SmsSender sender = SmsSender();
-    sender.sendSms(SmsMessage( phoneNumber, 'Fortfolio: Your OTP code is : $otp'));
-  }
+//   void sendOtp({required String phoneNumber}) {
+//     generateOtp();
+//     SmsSender sender = SmsSender();
+//     sender.sendSms(SmsMessage( phoneNumber, 'Fortfolio: Your OTP code is : $otp'));
+//   }
 
-  bool verifyOTP({required int smsCode}) {
-    return smsCode == otp;
-  }
+//   bool verifyOTP({required int smsCode}) {
+//     return smsCode == otp;
+//   }
 
-}
+// }
