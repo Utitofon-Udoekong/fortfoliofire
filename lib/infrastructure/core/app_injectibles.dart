f40b6e5448c0c30268188219d0c44d106877c6af
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-16 20:16:29 +0100

    ok

--- lib/infrastructure/core/app_injectibles.dart
+++ lib/infrastructure/core/app_injectibles.dart
@@ -3,7 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fortfolio/domain/auth/shared_pref_provider.dart';
 import 'package:injectable/injectable.dart';
+import 'package:telephony/telephony.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class FirebaseInjectableModule {
@@ -18,4 +21,7 @@ abstract class FirebaseInjectableModule {
 
   @lazySingleton
   FirebaseFunctions get functions => FirebaseFunctions.instance;
+  
+  @lazySingleton
+  Telephony get telephony => Telephony.instance;
 }
